a5c4b1311dbceb114a47c95b3711b4c98d6f6af9
Josh Levine <github@junk.josh.com>
2017-09-17 20:02:12 -0400

    sp

--- libraries/Examples01/examples/Blink/Blink.ino
+++ libraries/Examples01/examples/Blink/Blink.ino
@@ -10,7 +10,7 @@ void loop() {
 
   // a millisecond is 1/1,000th of a second, so
   // delay(500) will wait 1/2 a second, so
-  // this code will blink the tiel on and off
+  // this code will blink the tile on and off
   // once per second!
 
   setColor( RED );
