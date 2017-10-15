71683ef7462c25abab5db49a87428fc224d7d46f
Josh Levine <github@junk.josh.com>
2017-10-15 13:41:45 -0400

    Renamed error tester to datastream tester

--- libraries/Examples04/examples/IR_Datastream_Tester/IR_Datastream_Tester.ino
+++ libraries/Examples04/examples/IR_Datastream_Tester/IR_Datastream_Tester.ino
@@ -1,8 +1,9 @@
 /*
 
-    IR teser
+    IR Datstream tester
     
-    Test for errors on IR link by sending a pattern and then checking for missed steps on the receiver.
+    High level test for errors in a continuous data stream on the IR link by sending a pattern and then checking for missed 
+    steps on the receiver.
     
     On startup, we are in SEND MODE. We broadcast the pattern on all faces. Each step of the blue led
     indicates one step of the pattern. 
@@ -33,14 +34,6 @@
 #include "Arduino.h"
 
 #include "blinklib.h"
-
-//#include "ir.h"
-
-//#include "utils.h"
-
-//#include "util/delay.h"
-
-//#include "debug.h"
 
 void setup() {
     // This page intionally left blank
@@ -107,7 +100,7 @@ void loop() {
             
             // TODO: send all faces at once
             
-            irBroadcastData( count );
+            irBroadcastData(  count );
             delay(20);                  // Slow down a bit so the spin is not just a blur
                 
             break;
