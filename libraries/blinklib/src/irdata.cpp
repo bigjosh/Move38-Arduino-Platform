4c877c72a7b982125e2bf0d03d5d7910c21bdde3
Josh Levine <github@junk.josh.com>
2017-12-17 16:46:35 -0500

    Implmentation code done. Needs to be compiled and tested and documented.

--- libraries/blinklib/src/irdata.cpp
+++ libraries/blinklib/src/irdata.cpp
@@ -49,13 +49,6 @@
  
   #define IR_SPACE_TIME_US (600)  // Used for sending flashes. Must be longer than one IR timer tick including if this clock is slow and RX is fast. 
 
-// Time between consecutive sync flashes
-// A sync is 4 consecutive flashes that must be seen as the RX as at least 3
-
-//#define IR_SYNC_TIME_US (IR_WINDOW_US - (( ((unsigned long) IR_WINDOW_US * IR_CLOCK_SPREAD_PCT) ) / 100UL ) - TX_PULSE_OVERHEAD  )  // Used for sending flashes. Must be longer than one IR timer tick including if this clock is slow and RX is fast.
-
- #define IR_SYNC_TIME_US (400)  // Used for sending flashes. Must be longer than one IR timer tick including if this clock is slow and RX is fast.
-
 // from http://www.microchip.com/forums/m587239.aspx
 
 static uint8_t oddParity(uint8_t p) {
@@ -66,6 +59,8 @@ static uint8_t oddParity(uint8_t p) {
 }
 
 
+
+
 // You really want sizeof( ir_rx_state_t) to be  a power of 2. It makes the emitted pointer calculations much smaller and faster.
 
  typedef struct {
@@ -74,16 +69,19 @@ static uint8_t oddParity(uint8_t p) {
      
      // I think access to the  first element is slightly faster, so most frequently used should go here
      
-     uint8_t bitstream;               // The tail of the last sample window states. 
-     
-     uint8_t inputBuffer;                  // Buffer for RX in progress. Bits step up until high bit set.           
+    uint8_t windowsSinceLastFlash;          // How many times windows since last trigger? Reset to 0 when we see a trigger
+          
+    uint8_t inputBuffer;                    // Buffer for RX in progress. Data bits step up until high bit set.           
+                                            // High bit will always be set for real data because the start bit is 1
+                                           
 
     // Visible to outside world     
-     volatile uint8_t lastValue;      // Last successfully decoded RX value. 1 in high bit means valid and unread. 0= empty. 
-     
-     volatile uint8_t errorBits;      // Error conditions seen 
-          
-     // This struct should be even power of 2 long. 
+     volatile uint8_t lastValue;            // Last successfully decoded RX value. 1 in high bit means valid and unread. 0= empty. 
+
+    
+    uint8_t dummy;                          // TODO: parity bit? for now just keep struct a power of 2
+                                                           
+    // This struct should be even power of 2 long. 
               
  } ir_rx_state_t;
  
@@ -92,107 +90,6 @@ static uint8_t oddParity(uint8_t p) {
  // Data Indirect with Displacement opcodes
 
  ir_rx_state_t ir_rx_states[IRLED_COUNT];
-
-
-// Received a valid byte 
- 
- static void gotByte( ir_rx_state_t *ptr , const byte b ) {
-
-        if (ptr->lastValue) {   // Already have a value?
-
-            // Signal the overflow happened if anyone cares to check
-                    
-            SBI( ptr->errorBits , ERRORBIT_OVERFLOW );
-                    
-        }
-                
-        ptr->lastValue = b;                    // Save new value (overwrites in case of overflow)
-     
- }     
- 
-  
-// Got a valid bit. Deserialize it.
-
-static void gotBit(ir_rx_state_t *ptr, const bool bit ) {
-    
-    /*
-    // DEBUGB is decoded bitstream. Wide pulse is 1-bit.
-    if (bit)    DEBUGB_PULSE(40);
-    else DEBUGB_PULSE(20);    
-    */
-    
-    uint8_t buffer=ptr->inputBuffer;
-    
-    if (buffer) {           // There must be at least a leading 1-bit from the sync pulse or else we are not sync'ed
-        
-        if (buffer & 0b10000000) {     // We have already deserialized 7 bits, so this final bit is parity
-        
-            // TODO: Do we even need a parity check? Can any error really get though?
-            
-            if ( oddParity( buffer ) != bit ) {     // Parity checks (negate because of the leading non-data 1-bit
-
-                //DEBUGA_BITS( buffer );
-                
-                gotByte( ptr , buffer );
-
-                
-            }  else {
-
-                //DEBUGB_PULSE(50);
-
-                // Signal the parity error happened if anyone cares to check
-                
-                SBI( ptr->errorBits , ERRORBIT_PARITY );
-                    
-            }                     
-            
-            buffer=0;   // Start searching for next sync. 
-            
-        } else {  // buffer not full yet
-            
-            buffer <<= 1;
-            
-            buffer |= bit;
-            
-        }            
-        
-        ptr->inputBuffer=buffer;
-        
-    }        
-    
-    
-}    
-
-// Got a sync symbol. Start parsing bits.
-
-static void sync(ir_rx_state_t  *ptr) {
-    
-    ptr->inputBuffer=0b00000001;         // This will walk up to the 8th bit to single a full deserialization
-    
-}    
-
-
-// Start searching for sync again
-
-static void reset(ir_rx_state_t  *ptr, uint8_t errorReasonBit ) {
-    
-    ptr->inputBuffer=0;         // Start searching for next sync. 
-    SBI( ptr->errorBits , errorReasonBit );
-    
-    /*
-     
-    // Set scope to single trigger on DEBUGA channel to view reason for error. 
-    if (errorReasonBit==ERRORBIT_DROPOUT) {
-           DEBUGA_PULSE(100);
-       } else if (errorReasonBit==ERRORBIT_NOISE) {
-           DEBUGA_PULSE(200);
-       } else {
-           DEBUGA_PULSE(300);
-    }    
-    
-    */
-    
-}
 
 
 // Called once per timer tick
@@ -222,53 +119,49 @@ static void reset(ir_rx_state_t  *ptr, uint8_t errorReasonBit ) {
 
     while (bitwalker) {
         
-        bool bit = bits & bitwalker;
+        uint8_t bit = bits & bitwalker;
         
-        uint8_t bitstream = ptr->bitstream;
-        
-        bitstream <<= 1;                // Shift up 1 to make room for new bit
-        
-        bitstream |= bit;               // Save the new bit
-        
-        // The bitstream is a timeline of the last 8 samples, rightmost bit is the newest.
-        // A 1 here means that we saw an IR pulse in that sample. 
-        
-        // The reason that this looks a bit complicated is because the clocks of sender and receiver
-        // are not perfectly synced, so the sender can send two consecutive blinks but we 
-        // might see them with a space between them depending on how things line up. 
-        
-        // 1 blink is always a 0-bit
-        // 2 blinks is always a 1-bit
-        // 3 blinks is always a sync signal
-        // There is always 2 empty samples at the end of each of the above symbols. 
-              
-        // TODO: This can be massively optimized once we get the algorithm finalized.
-        // TODO: Maybe zero out top bits when we match so we don't need to mask with AND?
-       
-        
-        if ( (bitstream & 0b00011111) == 0b00000100 ) {     // Got a valid 0-bit symbol
-            gotBit( ptr , 0 );
-        } else if ( (bitstream & 0b00111111) == 0b00001100 ) {   // Got a valid 1-bit symbol (no gap between pulses)
-            gotBit( ptr , 1 );
-        } else if ( (bitstream & 0b01111111) == 0b00010100 ) {   // Got a valid 1-bit symbol (gap between pulses)
-            gotBit( ptr , 1 );
-        } else if ( (bitstream & 0b00000111) == 0b00000111 ) {   // Got a sync (can happen again on next sample when 4 consecutive pulses) 
-            sync( ptr );
-        } else {
+        if (bit) {      // This LED triggered in the last time window
             
-            // Only bother checking for errors if we are currently actually receiving a frame
-    
-            if (ptr->inputBuffer) {
-                if ( (bitstream & 0b00011111) == 0b00000000 ) {   // Too long since last trigger
-                    reset(ptr , ERRORBIT_DROPOUT );
-                    } else if ( (bitstream & 0b00011111) == 0b00010101 ) {   // Not a valid pattern. Noise.
-                    reset(ptr , ERRORBIT_NOISE);
+            uint8_t thisWindowsSinceLastFlash = ptr->windowsSinceLastFlash;
+            
+            ptr->windowsSinceLastFlash = 0;     // We just got a flash, so start counting over.
+            
+            if (thisWindowsSinceLastFlash<=3) {     // We got a valid bit
+                                                
+                uint8_t inputBuffer = ptr->inputBuffer;     // Compiler should do this optrimization for us, but it don't 
+                
+                inputBuffer <<= 1;      // Make room for new bit (fills with a 0)
+                            
+                if (thisWindowsSinceLastFlash<=1) {     // Saw a 1 bit
+                    
+                    inputBuffer |= 0b00000001;          // Save newly received 1 bit 
+                    
                 }
-            }            
-            
-        }                        
-       
-        ptr->bitstream = bitstream;
+                                
+                if ( inputBuffer & 0b10000000 ) {       // Do we already have a full 7-bit (plus start bit) byte in the buffer?
+                    
+                    // TODO: parity check would go here, but not for now. 
+                    
+                    // TODO: check for overrun in lastValue and either flag error or increase buffer size
+                    
+                    ptr->lastValue = inputBuffer;           // Save the received byte (clobbers old if not read yet)
+                    
+                    inputBuffer =0;                    // Clear out the input buffer to look for next start bit
+                    
+                } 
+                
+                ptr->inputBuffer = inputBuffer;
+                
+            }  else {
+                
+                // Received an invalid bit
+                
+                ptr->inputBuffer = 0;                       // Start looking for start bit again. 
+                                                    
+            }                
+                
+        }                
                          
         ptr--;                    
         bitwalker >>=1;
@@ -276,29 +169,6 @@ static void reset(ir_rx_state_t  *ptr, uint8_t errorReasonBit ) {
          
 }     
  
-
-// Read the error state of the indicated LED
-// Clears the bits on read
-
-uint8_t irGetErrorBits( uint8_t led ) {
-    
-    uint8_t bits;
-
-    ir_rx_state_t *ptr = ir_rx_states + led;        
-    
-    // Must do the snap-and-clear atomically because otherwise i bit might get set by the ISR 
-    // after we snapped, but before we cleared, and then then clear could clobber it. 
-    
-    DO_ATOMICALLY {
-    
-        bits = ptr->errorBits;        // snapshot current value    
-        ptr->errorBits=0;                      // Clear out the bits we are grabbing
-        
-    }     
-    
-    return bits; 
-}    
-
 // Is there a received data ready to be read on this face?
 
 bool irIsReadyOnFace( uint8_t led ) {
