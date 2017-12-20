37137e7f362ee8fb2b6a7733ca9637ae71103b7d
Josh Levine <github@junk.josh.com>
2017-12-20 02:26:35 -0500

    Stash. Good night.

--- AS7/blink/Sketches/Blink/main.cpp
+++ AS7/blink/Sketches/Blink/main.cpp
@@ -8,7 +8,6 @@
  */
 
 #include "blinklib.h"
-#include "blinkstate.h"
 
 #include "utils.h"
 #include "Serial.h"
@@ -17,62 +16,33 @@ Color colors[] = { RED , CYAN , MAGENTA, YELLOW, BLUE, GREEN };
 uint32_t timeLastChanged_ms = 0;
 uint16_t changeHoldoff_ms = 1000;
 
-ServicePortSerial sp;
+//ServicePortSerial sp;
 
 void setup() {
-  setState(1);
-  setColor(colors[0]);
-  
-  sp.begin();
-  
-  sp.print("For a good time, call:");
-  
-  const utils_serialno_t *sn = utils_serialno();
-  
-  // Embellished to print with dashes between bytes, trailing LF
-
-  for( int i=0 ; i<SERIAL_NUMBER_LEN-1; i++ ){
-    sp.print(  sn->bytes[i] , HEX );
-    sp.print('-');    
-  }      
+    setColor( BLUE );
     
-  sp.println(sn->bytes[i] , HEX );
 }
+
+uint32_t nextSend=0;
+
+uint8_t nextData =0; 
 
 void loop() {
     
-  int numNeighbors = 0;
-  uint32_t curTime_ms = millis();
-
-  if( buttonSingleClicked() ) {
-        setState( (getState() % 6) + 1 );
-        setColor( colors[getState()-1] );
-        timeLastChanged_ms = curTime_ms;
-  }
-
-  for( int i = 0; i < 6; i++ ) {
+    uint32_t now  = millis();
     
-    byte neighborState = getNeighborState(i);
+    if ( now > nextSend ) {
+        
+        irSendDataX(  nextData++ , 0b00111111 );
+        
+        nextSend = now + 1000;
+        
+        setColor( RED );
+                       
+    }        else {
+        
+        setColor( OFF );        
+    }        
     
-    if(neighborState != 0) {
-
-      numNeighbors++;
-      
-      if(neighborState != getState() && curTime_ms - timeLastChanged_ms > changeHoldoff_ms){
-        setState( neighborState );
-        setColor( colors[getState()-1] );
-        timeLastChanged_ms = curTime_ms;
-      }
-    }
-  }
-  
-  if(numNeighbors == 0) {
-    // alone, so glow white
-    setColor(WHITE);
-  }
-  else {
-    // together, show state
-    setColor( colors[getState()-1] );
-  }
 
 }
