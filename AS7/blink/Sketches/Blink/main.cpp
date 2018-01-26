73b35017d85dddb892c87e55c2742513315861be
Josh Levine <github@junk.josh.com>
2018-01-26 12:16:34 -0500

    Stash

--- AS7/blink/Sketches/Blink/main.cpp
+++ AS7/blink/Sketches/Blink/main.cpp
@@ -1,236 +1,361 @@
 /*
-    Happy New Years - 2018
-
-    Firecracker
-
-    Rules:
-    If button pressed...
-    explode with firecracker like flashing of lights that spreads to neighboring tiles
-        
-    if receive spark, explode like firecracker and  remember who I received spark from
-    
-    after an initial ignition time, pick one of my other neighbors to send the spark to.
-    we will not send a spark back to the same place we got it from. 
-    if no suitable neighbors available, firecracker is extinguished.
-
+* Speed up the rotation of LEDs lights or slow down based on button press
 */
 
 #include "blinklib.h"
 #include "blinkstate.h"
+#include "Serial.h"
 
-// Our local state. We also reuse these values as messages to send to neighbors
+ServicePortSerial Serial;
 
-enum state_t { 
-    READY ,         // We will explode if we see an INFECT message on any face. 
-    IGNITE ,        // Running the explosion pattern. soruceFace is the face we got SPARK from.
-    BURN,           // Spreading to any neighbor besides sourceFace.    
-};
+uint16_t speed_d_per_s = 360; // speed is represented in degrees/sec, 360 would be a single rotation per second
 
-static state_t state = READY; 
+float speed_d_per_ms = ( (float) speed_d_per_s) / MILLIS_PER_SECOND ;
 
-#define NO_FACE FACE_COUNT     // Use FACE_COUNT as a match-none special value for sourceFace
-                               // We use this when the explosion was manually started so no
-                               // face is the source. 
+enum team_t { TEAM_A , TEAM_B };
 
+byte teamHues[] = { 0 , 128 };
 
-static int sourceFace=NO_FACE; // The face we got the spark from.
-                               // Only valid in EXPLODING, COOLDOWN, and INFECT
-                               // NO_FACE if we did not get the spark from anywhere (was cause by button press)
+enum state_t { STATE_DEAD , STATE_START , STATE_NORMAL , STATE_WITHDRAW ,  STATE_ALONE , STATE_ATTACK };
 
-static int targetFace=NO_FACE; // The face we are sending the spark to.
-                               // Only matters in BURN state. NO_FACE if we are not spreading
+// We need a margin time both when withdrawing and when attacking
 
-Timer nextState;			   // Time we switch to next state. Valid in EXPLODING, COOLDOWN, and INFECT.
+// We need this to make sure there is a delay between when we stop seeing any neighbors and
+// when we start sending alone state- otherwise we could immediately attack neighbors as we pull
+// away from them
 
-// How long between when we first get a spark and start sending a new spark
-static const uint16_t igniteDurration_ms = 200;
+// We also need to make sure there is a delay between when we start attacking neighbors and when
+// we switch back to being normal to make sure we finish attacking our new neighbors
 
-// Time we will continue to burn after we start sparking
-static const uint16_t burnDuration_ms = 400;
+#define WITHDRAWAL_TIME_MS  200
+#define ATTACK_TIME_MS      200
+#define START_TIME_MS       400
 
+// We all come into this world DEAD.
+// A triple click puts us into NORMAL with HEALTH_START health.
+//  Health drops over time.
+//  When we see no neighbors, we go into WITHDRAWAL state.
+//  After we have been in withdrawal for a while, we become ALONE
+//  When in ALONE state, we start looking for neighbors,
+//    Once we see one, we start counting down DOCKING time
+//    At the end of DOCKING time period, we count the number of neighbors and add 5000*count to our health
+//  When in NORMAL state, if we see an ALONE nearby then we deduct 5000 from our health.
 
-// Show on an occupied face in READY state 
-static Color readyFaceColor = makeColorRGB( 10*8,  8*8 , 0 );
+// Health starts game at 60 seconds.
 
-// Show on the face that we are sending the spark to 
-static Color sparkFaceColor = makeColorRGB( 20*8 , 0 , 0 );
+uint32_t health;    // Units ms
 
-// Show on random faces as we ignite and burn
-static Color sparkleColor   = makeColorRGB( 20*8 , 20*8 , 20*8 );
+#warning shortend health for testing
+#define HEALTH_START 30000U   // Starting value of health
+//#define HEALTH_START 60000   // Starting value of health
+
+#define HEALTH_MAX  120000U     // We need a cap here to prevent overflow uint16
+
+#define HEALTH_ATTACK 5000U     // How many units we loose when attacked
+
+#define HEALTH_LOSS_PER_MS 1U   // We loose this many health each ms just from normal aging
+
+//#define HEALTH_MAX_DISPLAY 60000    // any health above this will just look full
+
+team_t team;
+
+state_t state;
+
+uint32_t timeOfLastLoop_ms;
 
 void setup() {
-  blinkStateBegin();
+	
+	Serial.begin();
+
+	Serial.println("Mortals Debug");
+
+	team = TEAM_A;
+
+	state = STATE_DEAD;
+
+	timeOfLastLoop_ms = millis();
+
 }
 
-// Returns a face that (1) has not yet expired, and (2) is not `exclude`
-// Returns NO_FACE is no faces meet the criteria
+// Sin in degrees ( standard sin() takes radians )
 
-static byte pickSparkTarget( byte exclude ) {
-    
-    // First tally up all the potential target faces
-                
-    byte potentialTargetList[FACE_COUNT];                 
-    byte potentialTargetCount=0;
-                
-    FOREACH_FACE(f) {
-        
-        if ( (!isNeighborExpired(f)) && (f!=exclude) ) {
-            
-            potentialTargetList[ potentialTargetCount ] = f;
-            potentialTargetCount++;
-            
-        }
-        
-    }
-    
-    if ( potentialTargetCount==0) {
-        // No place to send
-        return NO_FACE;
-    }        
-        
-    // Pick which of the potential nodes is the winner...
-                    
-    byte targetIndex = rand( potentialTargetCount-1 );
-    
-    return potentialTargetList[ targetIndex ];
-    
-}    
-    
+float sin_d( uint16_t degrees ) {
+
+	return sin( ( degrees / 360.0F ) * 2.0F * PI   );
+}
+
+float rotation_d = 0;     // Spin display over time
+
+byte lastValue[FACE_COUNT];     // Keep track of last seen value on each face so we can tell if it changed
+
+// Reduce health with 0 floor
+
+void reduceHealthBy( uint16_t x ) {
+	
+	if ( health > x ) {
+		health -= x;
+		} else {
+		health = 0;
+	}
+}
+
+// Increase health with HEALTH_MAX ceiling
+
+void increaseHealthBy( uint16_t x ) {
+	
+	if ( health < ( HEALTH_MAX - x )) {
+		health += x;
+		} else {
+		health = HEALTH_MAX;
+	}
+}
+
+// loop like we are Elm main:
+// Establish inputs (button,neighbors)
+// Update state (health,state)
+// Produce outputs (display , transmitted state)
+
+Timer nextState; 
 
 void loop() {
-        
-         
-    // put your main code here, to run repeatedly:
+	
+	uint32_t now = millis();
 
-  
-    bool detonateFlag = false;        // Set this flag to true to cause detonation
-                                      // since the ignition can come from either button or spark
-                                      // and we don't care which
-  
-    // First get some situational awareness
-    
-  
-    FOREACH_FACE(f) {
-        
-        
-        if (neighborStateChanged(f)) {
+	if (buttonLongPressed()) {      // Switch team (displayed color) on long press
 
-            byte receivedMessage = getNeighborState(f);    
-            
-            if (receivedMessage==BURN) {      // We just got a spark!
-                
-                detonateFlag=true;
-                sourceFace=f;
-                
-            } else if (receivedMessage==IGNITE) {
-                
-                // Looks like the target we were trying to light did catch fire!
-                
-                // No need to keep sending to them
-                
-                targetFace=NO_FACE;
-                
-            }                
-                                            
-        }                          
-    }      
-    
-    
-    // Next update our state based on new info 
-        
-    // if button pressed, firecracker
-    if (buttonSingleClicked()) {
-        detonateFlag=true;
-        sourceFace = NO_FACE;           // Manually initiated, so no source                
-    }
-              
-  
-    if (detonateFlag) {
-        state=IGNITE;
-        nextState.setMSFromNow( igniteDurration_ms );
-    }      
-      
-    if ( nextState.isExpired() ) {        // Time for next timed state transition?
-        
-        // These are the only states that can timeout
-        
-        if (state==IGNITE) {
-            
-            // We are ready to spread the flame!
-            
-            // Try to pick one. Will return NO_FACE if none possible.
-            
-            targetFace = pickSparkTarget( sourceFace );
-            
-            state=BURN;
-            nextState.setMSFromNow( burnDuration_ms );
-            
-        } else if (state==BURN) {              // Technically don't need this `if` since this is the only possible case, but here for clarity.             
-            
-            state=READY;
-            nextState.setNever();                 
-            
-        }                
-                
-    }            
-  
-  
-    // In all states, we show orange on any face that has a neighbor
-    // (may be covered by effect if we are EXPLODING) 
-    
-    FOREACH_FACE(f) {
-          
-        if (!isNeighborExpired(f)) {
-            setFaceColor(f, readyFaceColor );
-        } else {
-            setFaceColor(f, OFF );
-        }
-          
-    }
-       
-    // If there is an explosion in progress, blink a random face 
-    // will draw over the orange set above. 
-    
-    if (state == IGNITE || state == BURN ) {         
-        // Blink a random face white
-        setFaceColor( rand( FACE_COUNT -1 ) , sparkleColor );            
-    }        
-    
-    // Finally we set out outputs
-        
-    // We send out current state on all faces EXCEPT for the special
-    // case when we are INFECTing, in which case we want to only send that 
-    // on the target face and look READY to everyone else. 
-        
-    if (state==BURN) {
-        
-        // Burn state is special since we only send the burn to the target neighbor
-        // or else all our neighbors would catch on fire
+		if (team==TEAM_A) {
+			team=TEAM_B;
+			} else {
+			team=TEAM_A;
+		}
+		
+		
+	}
 
+	if (buttonMultiClicked()) {     // Restart on triple click
 
-        // This is a bit of a hack. We can't send our current state to everyone because then
-        // we would infect everyone. So we send READY because it is benign.        
-        setState( READY ); 
-        
-        if (targetFace != NO_FACE ) {
-                   
-            // Show the spark flying away
-            setFaceColor( targetFace , sparkFaceColor );            
-                                       
-            // We do explicitly send BURN to the target we are aiming to infect.
-            setState( state , targetFace );
-            
-        }  
-        
-    } else {
-            
-        // For any other state besides BURN, we can just send our real state to everyone
-        // Note that when we send IGNITE, the person who sent us burn will see it and
-        // know that we ignited and will stop sending BURN out anymore. 
-            
-        setState( state );
-    }        
-                                       
+		state = STATE_START;
+		health = HEALTH_START;
+		
+		nextState.setMSFromNow( START_TIME_MS ); // Wait a bit to let neighbors start too before we attack each other!
+		Serial.println("START");
+		
+	}
+
+	uint32_t timeDiff_ms = now - timeOfLastLoop_ms;
+	
+	// First see what is going on around our edges
+	
+	bool aloneFlag = true;          // Assume we are alone until we see neighbors
+	byte newAttackers=0;            // Anyone on any face attack us that was not attacking last pass?
+	byte newNormals=0;
+	
+	FOREACH_FACE(f) {
+				
+		byte newNeighboorState = getNeighborState( f );
+		
+		if ( !isNeighborExpired(f) ) {
+			
+			aloneFlag = false;                  // We may not be loved, but we are not alone
+		}
+		
+		if ( newNeighboorState != lastValue[f] ) {            // Changed?
+			
+			if ( newNeighboorState == STATE_ATTACK ) {         // New attacking neighbor!
+				
+				newAttackers++;
+				
+			}
+			
+			if ( newNeighboorState == STATE_NORMAL ) {
+				
+				newNormals++;
+				
+			}
+			
+			lastValue[f] = getNeighborState(f);
+		}
+	}
+	
+	
+	switch (state) {
+		
+		case STATE_DEAD:
+		
+		// Dead is as dead does
+		break;
+		
+		case STATE_START:
+		
+		if ( now > state_timeout_ms ) {
+			
+			state = STATE_NORMAL;
+			Serial.println("NORMAL");
+			
+			
+		}
+		
+		break;
+		
+		case STATE_NORMAL:
+		
+		// Is this correct behavior? Is it even even valid to have more than one attacker?
+		
+		reduceHealthBy( HEALTH_ATTACK * newAttackers );      // Will not go below 0
+		
+		if (newAttackers) {
+			Serial.print("Attacked by ");
+			Serial.println(newAttackers);
+		}
+		
+		if (aloneFlag) {
+			
+			// We are newly alone, so start withdrawing...
+			
+			state = STATE_WITHDRAW;
+			state_timeout_ms = now + WITHDRAWAL_TIME_MS;
+			
+			Serial.println("WITHDRAW");
+			
+		}
+		
+		break;
+		
+		case STATE_WITHDRAW:
+		
+		if ( now > state_timeout_ms ) {
+			
+			state = STATE_ALONE;
+			Serial.println("ALONE");
+			
+			
+		}
+		
+		break;
+		
+		case STATE_ALONE:
+		
+		if (!aloneFlag) {            // Not alone any more!
+			
+			state = STATE_ATTACK;
+			state_timeout_ms = now + ATTACK_TIME_MS;
+			Serial.println("ATTACK");
+
+			
+		}
+		
+		increaseHealthBy( newNormals * HEALTH_ATTACK );
+
+		if (newNormals) {
+			Serial.print("Initial Stole from ");
+			Serial.println(newNormals);
+		}
+		
+		
+		break;
+		
+		case STATE_ATTACK:
+		
+		increaseHealthBy( newNormals * HEALTH_ATTACK );
+
+		if (newNormals) {
+			Serial.print("Ongoing Stole from ");
+			Serial.println(newNormals);
+		}
+
+		
+		if ( now > state_timeout_ms ) {
+			
+			state = STATE_NORMAL;
+			Serial.println("NORMAL");
+
+			
+		}
+		
+		break;
+
+	}
+	
+	// Next we age and lose health and maybe die
+	
+	if (state != STATE_DEAD ) {
+
+		if ( timeDiff_ms > health ) {
+
+			// Oh no! We died!
+
+			state = STATE_DEAD;
+
+			setColor( WHITE );        // Death flash
+
+			delay(100);
+			
+			} else {
+
+			reduceHealthBy( timeDiff_ms * HEALTH_LOSS_PER_MS );
+			
+		}
+	}
+	
+	// Next update the display to reflect our current health
+	
+	if (state == STATE_DEAD ) {
+		
+		setColor(OFF);
+		
+		} else { // state != DEAD
+		
+		// calculate the amount of rotation
+		
+		//speed_d_per_ms = ( ((1.0f - (min(  health  , HEALTH_MAX_DISPLAY ) / HEALTH_MAX_DISPLAY ))) * 4.0 * 360.0) / MS_PER_S;
+		
+		//speed_d_per_ms = ((1.0f - ( (HEALTH_MAX_DISPLAY/1.5) / HEALTH_MAX_DISPLAY )) * 4.0 * 360.0) / MS_PER_S;
+		
+		
+		rotation_d += timeDiff_ms * speed_d_per_ms ;
+		
+		if(rotation_d >= 360.f) {
+			rotation_d -= 360.f;
+		}
+		
+		//byte angularWidth_d =  max(  health  , HEALTH_MAX_DISPLAY ) *  360  / HEALTH_MAX_DISPLAY;
+		
+		//byte brightness =  min(  health  , HEALTH_MAX_DISPLAY ) *  255  / HEALTH_MAX_DISPLAY;
+		
+		FOREACH_FACE(f) {
+			// determine brightness based on the unit circle (sinusoidal fade)
+			
+			uint16_t angle_of_face_d = 60 * f;      // (360 degrees) / (6 faces) = degrees per face
+			
+			uint16_t angle_of_health_d =  (min(  health  , 30000 ) / 30000.0) * 360;
+			
+			byte brightness;
+			
+			if ( angle_of_face_d < angle_of_health_d ) {
+				
+				brightness = 255;
+				}   else {
+				brightness = 0;
+			}
+			
+			//int brightness = 255 * (1 + sin_d( angle_of_face_d  + rotation_d ) ) / 2;
+			
+			setFaceColor( f ,  makeColorHSB( teamHues[team]  , 255 , brightness  ) );
+			
+			
+			//      if(f==4) {
+			//        Serial.print("rotation: ");
+			//        Serial.println(rotation);
+			//        Serial.print("brightness: ");
+			//        Serial.println(brightness);
+			//      }
+			
+		}
+		
+	}    // state != DEAD
+
+	timeOfLastLoop_ms = now;
+
+	setState( state );
+	
 }
-
-
