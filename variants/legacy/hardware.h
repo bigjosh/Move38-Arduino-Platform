f366bf4b1bf5d596c08edb7d68edf6f5d97c9dab
Josh Levine <github@junk.josh.com>
2018-03-20 11:29:40 -0400

    Bring legacy blinks variant hardware.h up to speed

--- variants/legacy/hardware.h
+++ variants/legacy/hardware.h
@@ -1,13 +1,11 @@
 /*
  * hardware.h
  *
- * Defines the location of all the hardware 
- * This header matches all boards that do NOT have the pretty logo on the front. 
- * Less than 100 boards like this, so someday hopefully we can retire this header.
+ * Defines the location of all the hardware
+ * This header matches all boards that *DO* have the pretty logo on the front (newer than 1/1/17).
+ * Less than 100 boards older than this, hopefully we can retire the legacy header and move this to core
  *
- *
- * Created: 7/23/2017 9:50:54 PM
- */ 
+ */
 
 
 #ifndef HARDWARE_H_
@@ -15,8 +13,7 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "utils.h"
-
+//#include "utils.h"
 
 /*** PIXELS ***/
 
@@ -72,7 +69,7 @@
 #define BLUE_SINK_DDR  DDRE
 #define BLUE_SINK_BIT  3
 
-/*** IR ***/ 
+/*** IR ***/
 
 // IR transceivers
 // There are 6 IR LEDs - one for each face
@@ -87,7 +84,7 @@
 
 
 // CONSTRAINTS:
-// Note that all IR anodes must be on same port, as do all IR cathodes. 
+// Note that all IR anodes must be on same port, as do all IR cathodes.
 // Cathodes must have pin change interrupt
 //
 // Each anode must be on the same bit as the corresponding anode. (this could be relaxed with extra code)
@@ -97,10 +94,10 @@
 #define IR_BITS     (_BV( 0 )|_BV( 1 )|_BV( 2 )|_BV( 3 )|_BV( 4 )|_BV( 5 ))
 
 // IR pin change interrupts are unused so far, but we will want them for waking from sleep soon...
-// TODO: Wake on IR change. 
+// TODO: Wake on IR change.
 
-// We want a pin change interrupt on the CATHODES since these will get charged up 
-// and then exposure will make them go low. 
+// We want a pin change interrupt on the CATHODES since these will get charged up
+// and then exposure will make them go low.
 // PORTC is connected to the cathodes, and they are on PCINT0-PCINT5
 // which is controlled by PCIE0
 
@@ -124,10 +121,9 @@
 */
 
 
-#define IR_PCI     PCIE1
-#define IR_ISR     PCINT1_vect
-#define IR_MASK    PCMSK1           // Each bit here corresponds to 1 pin
-#define IR_PCINT   IR_BITS
+#define IR_PCI_BIT      PCIE1            // Set this bit in PCICR to enable pin change INT on pins PCINT8-14 - the cathode pins on PORTC
+#define IR_ISR          PCINT1_vect      // The vector called when pin change happens on cathode pins
+#define IR_INT_MASK_REG PCMSK1           // Each bit here corresponds to 1 cathode pin
 
 /*** Button ***/
 
@@ -143,7 +139,6 @@
 #define BUTTON_DOWN() (!TBI(BUTTON_PIN,BUTTON_BIT))           // PCINT23 - pulled low when button pressed
 
 
-/*** SERCIVE PORT ***/
 
 // Do not define SP_PRESENT
 
@@ -151,8 +146,7 @@
  // we are stuck defining these even though they are wrong. If we do not, then ARDUINO still compiles
  // the Serial class even if we do not include it and that causes errors when it pulls in the sp.x files. 
 
- 
- // Service port hardware
+// Service port hardware
 
 // Pins as digital IO
 
@@ -168,11 +162,10 @@
 #define SP_T_DDR  DDRD
 #define SP_T_BIT  1
 
-// Serial port hardware on service port 
+// Serial port hardware on service port
 
 #define SP_SERIAL_CTRL_REG      UCSR0A
 #define SP_SERIAL_DATA_REG      UDR0
 #define SP_SERIAL_READY_BIT     RXC0
-
 
 #endif /* HARDWARE_H_ */
